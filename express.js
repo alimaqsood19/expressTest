117e9be57dbd30228646df4fa359a94232e047c9
Ali Maqsood <ali-maqsood@hotmail.com>
2018-09-06 21:14:52 -0400

    Server init

---
+++ express.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+var PORT = 3000;
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.listen(PORT, function() {
+  console.log('Running on PORT ' + PORT);
+});
